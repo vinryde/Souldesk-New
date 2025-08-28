0e6777e9c1019e57c80f72d6d7003123911cfa11
vinryde <vinryde66@gmail.com>
2025-08-28 19:41:32 +0530

    done good

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ❌ This will completely skip ESLint checks when you run `next build`
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
